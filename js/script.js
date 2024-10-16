5d96669152e3c05c63bf175f81424c439c2f8bc9
HBR <56914134+HUNBUNNARITH@users.noreply.github.com>
2024-10-16 23:29:39 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Remove the alert when the page loads
+window.onload = function() {
+    // alert("Welcome to the Protected Area Management System!"); // This line is removed
+};
+
+// Example function to handle navigation (to be developed further)
+function navigateTo(page) {
+    window.location.href = page;
+}
